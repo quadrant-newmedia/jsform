867dcf73e98ac080e5944a09a1ba0d013887b90c
Alex Fischer <alex@quadrant.net>
2020-05-28 14:11:22 -0600

    removed demos

--- alert_something.js
+++
@@ -1,1 +0,0 @@
-alert('You submitted the form! The input value is: '+form.userinput.value);
--- alert_something_multiple.js
+++
@@ -1,2 +0,0 @@
-alert('You submitted the form! The input value is: '+form.userinput.value);
-return true;
